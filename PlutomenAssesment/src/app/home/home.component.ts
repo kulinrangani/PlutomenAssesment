ee6b6b24919196d4b8e98df1f1a1d5927129fc69
kulinrangnai <kulinrangani01@gmail.com>
2023-08-08 21:19:11 +0530

    Added library list to home page

--- PlutomenAssesment/src/app/home/home.component.ts
+++ PlutomenAssesment/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
-
+  constructor(private http: HttpClient) {}
+  libraries: any;
+  listOfLibraries: any[] = [];
+  reqUrl: string = 'https://api.workflowdev.pluto-men.com';
   ngOnInit(): void {
     let userJson: any = JSON.parse(localStorage.getItem('token') || '');
     localStorage.setItem('token', JSON.stringify(userJson.accessToken));
+    let t: string = JSON.parse(localStorage.getItem('token') || '');
+    var headers_object = {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${t}`),
+    };
+
+    this.http
+      .get(`${this.reqUrl}/backend/libraries/1/10`, headers_object)
+      .subscribe(
+        (res) => {
+          this.libraries = res;
+          this.listOfLibraries = this.libraries.docs;
+          console.log(this.listOfLibraries);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 }
